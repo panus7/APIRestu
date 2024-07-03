557299d1e872e6d91751f13f8853514258d152d3
Panus Chachon <dev.pnz7@gmail.com>
2024-07-03 08:30:47 +0700

    add table struck

---
+++ APIRestService/TableStructer_Restu.h
@@ -0,0 +1,45 @@
+﻿
+TABLE=ORDER_HEAD
+FIELD=OrderNo,			TYPE=varchar,SIZE=60 //TableID-TimeTick
+FIELD=TableID,			TYPE=varchar,SIZE=20
+FIELD=OrderDateTime,	TYPE=datetime
+FIELD=OrderByUserID,	TYPE=varchar,SIZE=20
+FIELD=CxlDateTime,		TYPE=datetime
+FIELD=CxlUserID,		TYPE=varchar, SIZE = 20
+FIELD=CookAckDateTime,	TYPE=datetime
+FIELD=CookAckUserID,	TYPE=varchar,SIZE=20
+FIELD=ReceiveDateTime,	TYPE=datetime
+FIELD=ReceiveUserID,	TYPE=varchar,SIZE=20
+PRIMARYKEY=OrderNo
+
+TABLE=ORDER_DETAIL
+FIELD=OrderNo,			TYPE=varchar,SIZE= 60 //TableID-TimeTick
+FIELD=Suffix,			TYPE=int
+FIELD=MenuID,			TYPE=varchar, SIZE=30
+FIELD=MenuMemo,			TYPE=varchar, SIZE=300
+FIELD=CookAckDateTime,	TYPE=datetime
+FIELD=CookAckUserID,	TYPE=varchar, SIZE=20
+FIELD=CxlDateTime,		TYPE=datetime
+FIELD=CxlUserID,		TYPE=varchar, SIZE=20
+FIELD=ChargeAmt,		TYPE=float
+PRIMARYKEY=OrderNo,Suffix
+
+TABLE=RECEIVE_HEAD
+FIELD=RecevieNo,		TYPE=varchar, SIZE=60 //R+TableID-TimeTick
+FIELD=TableID,			TYPE=varchar, SIZE=20
+FIELD=CxlDateTime,		TYPE=datetime
+FIELD=CxlUserID,		TYPE=varchar, SIZE=20
+FIELD=ReceiveDateTime,	TYPE=datetime
+FIELD=ReceiveUserID,	TYPE=varchar, SIZE=20
+FIELD=ChargeAmt,		TYPE=float
+FIELD=DiscountAmt,		TYPE=float
+FIELD=TotalAmt,			TYPE=float
+PRIMARYKEY=RecevieNo
+
+TABLE=RECEIVE_DETAIL
+FIELD=RecevieNo,		TYPE=varchar, SIZE=60 //TableID + TimeTick
+FIELD=Suffix,			TYPE=int
+FIELD=OrderNo,			TYPE=varchar, SIZE=30
+FIELD=ChargeAmt,		TYPE=float
+PRIMARYKEY=RecevieNo,Suffix
+ 
